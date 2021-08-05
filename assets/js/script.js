6a35d8a4ef55195765a507d7f30d81c8e8f48073
Tony Rivera <ajboricua@gmail.com>
2021-08-05 12:27:54 -0400

    updated api and code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,57 @@
+// global variable
+var searchHistory = []
+var apiKey = 'e76a8c755ca4d99e1a067cbf7bed9cc8'
 
-// create a bootstrap card in js - with same ID for weather results
 
-document.getElementById('temp')
-document.getElementById('wind')
-document.getElementById('humid')
-document.getElementById('uv')
+ // create a bootstrap card in js - with same ID for weather results
+
+ document.getElementById('temp')
+ document.getElementById('wind')
+ document.getElementById('humid')
+ document.getElementById('uv')
+
 
 // create a bootstrap card in js - with same ID for 5 day forcaste
 document.getElementById('forecast cards')
 
-// will loop each forecast 
-// array.forEach(element => {
-    
-// });
-
-// create a variable for cities searches
-var searchHistory = []
+// dom elements
+var searchHistoryContainer = document.querySelector('#history')
+var searchInput = document.querySelector('#cityInput')
 
 
+function renderHistory() {
+    searchHistoryContainer.innerhtml=''
+    for (let index = searchHistory.length -1; index >= 0; i--) {
+            var btn = document.createElement('button')
+            btn.setAttribute('type', 'button')
+            btn.classList.add('history-btn', 'btn-history')
+            btn.setAttribute('data-search', searchHistory[i])
+            btn.textContent = searchHistory[i];
+            searchHistoryContainer.append('btn');
+    }
+}
+
+function appendHistory(search) {
+    if (searchHistory.indexOf(search) != -1 ) {
+        return
+    }
+    searchHistory.push(search)
+    localStorage.setItem('search-history', JSON.stringify(searchHistory));
+    renderHistory();
+}
+    function initiateSearchHistory(){
+        let storeHistory = localStorage.getItem('search-history')
+        if (storeHistory) {
+            searchHistory = JSON.parse(storeHistory)    
+        }
+        
+        renderHistory()
+    }
+
+
+    // create a dom to generate the days on the div 
+    // dom create element div
+    // dom create element 
 
 // integrating the api for two weather forecast 
 
@@ -35,13 +69,3 @@ $('.search-city').on('click', function() {
     .then(data => console.log(data))
 })
 
-// fetch() forcast for 5 days 
-
-// // add local storage
-// localStorage.setItem()
-
-// // have a function that will search 
-//     search function()
-
-// // append to history 
-// append. to search history
